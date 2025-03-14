24ba87c002e1698467cc4c2fa895509b54e71e2c
Rowel Maharjan <rowelmhj@gmail.com>
2025-03-14 18:25:20 +0545

    Editor added

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,7 +1,18 @@
+import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
 import React from "react";
 
 const Document: React.FC = () => {
-  return <div>Document</div>;
+  return (
+    <div>
+      <Header>
+        <div className="flex w-fit items-center justify-center gap-2">
+          <p className="document-title">This is document title</p>
+        </div>
+      </Header>
+      <Editor />
+    </div>
+  );
 };
 
 export default Document;
